24d14b92840b1d444efa45988df81da55dfeff39
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-24 09:24:32 +0700

    menambahkan kode  untuk memulai server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express');
-const connectDatabase = require('./config/db');
-
 const app = express();
-const POST = 3000;
+const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log('Server running at http://localhost:$(PORT)');
+    });
+}
+
+startServer();
